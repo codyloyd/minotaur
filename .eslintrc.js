47a2648b7436a3a451cc3b53d7d56bd1721c4b77
Cody Loyd <codyloyd@gmail.com>
2020-05-11 13:12:24 -0500

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
+};
