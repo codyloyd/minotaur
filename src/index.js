dd3ae5578a7f4b56a2b8d8e6857f550d280cff31
Cody Loyd <codyloyd@gmail.com>
2020-05-11 13:12:44 -0500

    first draft

---
+++ src/index.js
@@ -0,0 +1,53 @@
+class Game {
+  constructor({width, height, scale, parent}) {
+    this.width = width
+    this.height = height
+    this.parent = document.getElementById(parent)
+
+    this.canvas = document.createElement('canvas')
+    this.canvas.width = this.width * scale 
+    this.canvas.height = this.height * scale 
+
+    this.context = this.canvas.getContext('2d')
+    this.context.scale(scale, scale)
+
+    this.parent.appendChild(this.canvas)
+
+    this.oldTimeStamp = 0
+    this.running = false
+  }
+
+  update() {
+    throw(new Error('must implement `update` function'))
+  }
+
+  draw() {
+    throw(new Error('must implement `draw` function'))
+  }
+
+  start() {
+    window.requestAnimationFrame(this.gameLoop.bind(this))
+    this.running = true
+  }
+
+  stop() {
+    this.running = false
+  }
+
+  gameLoop(timeStamp) {
+    // calculate deltaTime
+    const deltaTime = (timeStamp - this.oldTimeStamp)
+    this.oldTimeStamp = timeStamp
+
+    this.update(deltaTime/1000)
+    this.draw()
+
+    if (this.running) {
+      window.requestAnimationFrame(this.gameLoop.bind(this))
+    }
+  }
+}
+
+module.exports = {
+  Game
+}
